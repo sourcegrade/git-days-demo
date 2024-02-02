9dfbdab59c324f77f01e1c55694854c1664fd6b2
Alexander Städing <alexanderstaeding@gmail.com>
2024-02-02 10:45:16 +0100

    Add fopbot dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 }
 
 dependencies {
+    implementation("org.tudalgo:fopbot:0.7.1")
     testImplementation(platform("org.junit:junit-bom:5.10.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
